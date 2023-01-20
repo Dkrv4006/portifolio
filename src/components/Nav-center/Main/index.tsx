96bb0e359a3ea06d41d4811d062d6b9e0a77d4c1
Dkrv4006 <danielkennedy400619@gmail.com>
2023-01-20 03:13:09 -0300

    Update index.tsx

--- src/components/Nav-center/Main/index.tsx
+++ src/components/Nav-center/Main/index.tsx
@@ -16,14 +16,14 @@ export const Main: React.FC = () => {
       name:'JavaScript',
       img: javascript,
       link:"/project",
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus perferendis minus laudantium eveniet neque repellat dolor accusamus odit? Voluptatum expedita veniam illo ea. Nisi, corporis odit beatae blanditiis veniam asperiores.'
+      text: 'Javascript é usado por programadores em todo o mundo para criar conteúdo web dinâmico e interativo, como aplicativos e navegadores.'
     },
     {
       id:1 ,
       name:'TypeScript',
       img: typescript,
       link:"/project",
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus perferendis minus laudantium eveniet neque repellat dolor accusamus odit? Voluptatum expedita veniam illo ea. Nisi, corporis odit beatae blanditiis veniam asperiores.'
+      text: 'TypeScript é uma linguagem de programação fortemente tipada que se baseia em JavaScript, oferecendo melhores ferramentas em qualquer escala.'
 
     },
     {
@@ -31,15 +31,14 @@ export const Main: React.FC = () => {
       name:'ReactJs',
       img: reactjs,
       link:"/project",
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus perferendis minus laudantium eveniet neque repellat dolor accusamus odit? Voluptatum expedita veniam illo ea. Nisi, corporis odit beatae blanditiis veniam asperiores.'
-
+      text: 'React faz com que a criação de UIs interativas seja uma tarefa fácil. Crie views simples para cada estado na sua aplicação, e o React irá atualizar e renderizar de forma eficiente apenas os componentes necessários na medida em que os dados mudam.'
     },
     {
       id:1 ,
       name:'NodeJs',
       img: nodejs,
       link:"/project",
-      text:  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus perferendis minus laudantium eveniet neque repellat dolor accusamus odit? Voluptatum expedita veniam illo ea. Nisi, corporis odit beatae blanditiis veniam asperiores.'
+      text:  'O Node.js é um ambiente de servidor de código aberto e multiplataforma que pode ser executado no Windows, Linux, Unix, macOS e muito mais. Node.js é um ambiente de tempo de execução JavaScript back-end, é executado no V8 JavaScript Engine e executa código JavaScript fora de um navegador da Web.'
 
     },
     {
