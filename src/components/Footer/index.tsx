e5d87e11a68487d4aa7b70f9acaedccb755aafc7
Dkrv4006 <danielkennedy400619@gmail.com>
2023-01-27 08:23:56 -0300

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { Container  } from "./style"
 export const Footer: React.FC = () => {
   return (
     <Container>
-      <h3 className="h3" > <span>©</span> 26/01/2022</h3>
+      <h3 className="h3" > <span>©</span>2022</h3>
       <h3 className="h3"><span>{'{'}</span> Daniel Kennedy <span>{'}'}</span></h3>
       <h3 className="h3">danielkennedypro01@gmail.com</h3>
 
